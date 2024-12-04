82c264ac218a17abaef5a4ba24cf6444fc325e87
Nikulin Pavel <p.a.nikulin@tinkoff.ru>
2024-12-04 18:36:33 +0300

    Add photo grid

---
+++ images/photo_grid/optimize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ffmpeg -i 01.png -preset ultrafast 01.jpg
+ffmpeg -i 02.png -preset ultrafast 02.jpg
+ffmpeg -i 03.png -preset ultrafast 03.jpg
+ffmpeg -i 04.png -preset ultrafast 04.jpg
+ffmpeg -i 05.png -preset ultrafast 05.jpg
+ffmpeg -i 06.png -preset ultrafast 06.jpg
+ffmpeg -i 07.png -preset ultrafast 07.jpg
+ffmpeg -i 08.png -preset ultrafast 08.jpg
+ffmpeg -i 09.png -preset ultrafast 09.jpg
+ffmpeg -i 10.png -preset ultrafast 10.jpg
+ffmpeg -i 11.png -preset ultrafast 11.jpg
+ffmpeg -i 12.png -preset ultrafast 12.jpg
