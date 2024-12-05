3cacd53e8c93f4375608fdec496f03cf9c06fbda
Nikulin Pavel <p.a.nikulin@tinkoff.ru>
2024-12-05 21:21:09 +0300

    Start doing places

---
+++ images/places/optimize.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ffmpeg -i 01.png -preset ultrafast 01.jpg
+ffmpeg -i 02.png -preset ultrafast 02.jpg
+ffmpeg -i 03.png -preset ultrafast 03.jpg
+ffmpeg -i 04.png -preset ultrafast 04.jpg
+ffmpeg -i 05.png -preset ultrafast 05.jpg
